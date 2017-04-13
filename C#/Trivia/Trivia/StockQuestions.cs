c8bd7a3b7c549107b923cb2471f8d009877b34d0
etreille <tonashss@hotmail.fr>
2017-04-13 15:37:48 +0200

    ajout de la classe StockQuestions

---
+++ C#/Trivia/Trivia/StockQuestions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Trivia
+{
+    internal class StockQuestions
+    {
+        private readonly LinkedList<string> stock = new LinkedList<string>();
+
+        public void addQuestion(string question)
+        {
+            stock.AddLast(question);
+        }
+
+        public string getQuestion()
+        {
+            string question =stock.First();
+            this.removeQuestion();
+            return question;
+        }
+
+        public void removeQuestion()
+        {
+            stock.RemoveFirst();
+        }
+    }
+}
